22351b112d9aca1de1b09f0b517bac0d6689ccde
Zsolt Oszvald <ozsolt001@gmail.com>
2026-06-09 18:41:15 +0200

    Creating the first two cards on the home page as a prototype

--- Frontend/src/app/components/home/home.ts
+++ Frontend/src/app/components/home/home.ts
@@ -8,4 +8,39 @@ import { OpenmojiIconComponent } from '../../shared/openmoji-icon/openmoji-icon'
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+
+activity = [
+  {
+    id: 1,
+    title: 'Futás',
+    description: '5 km futás',
+    icon: '1F3C3-200D-2642-FE0F-200D-27A1-FE0F'
+  }, 
+  {
+    id: 2,
+    title: 'Úszás',
+    description: '30 perc úszás',
+    icon: '1F3CA'
+  }
+];
+
+selectedActivities: any[] = [];
+
+toggleActivity(activity: any): void {
+  const exists = this.selectedActivities.some(x => x.id === activity.id);
+
+  if (exists) {
+    this.selectedActivities = this.selectedActivities.filter(x => x.id !== activity.id);
+  } else {
+    this.selectedActivities = [...this.selectedActivities, activity];
+  }
+
+  console.log(this.selectedActivities);
+}
+
+isSelected(activity: any): boolean {
+  return this.selectedActivities.some(x => x.id === activity.id);
+}
+  
+}
